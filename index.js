c0ae995db666f1c5c04490678170ccba9416edfa
Pascal Martineau <pascal.animateur@gmail.com>
2016-05-11 11:15:40 -0400

    Use internal webpack config instead of sails.config.vue

--- index.js
+++ index.js
@@ -12,8 +12,64 @@ module.exports = function (sails) {
       }
     },
 
+    config: {
+      entry: [path.resolve(__dirname, '../../src/main.js')],
+      output: {
+        path: path.resolve(__dirname, '../../assets/'),
+        filename: 'js/build/bundle.js',
+        publicPath: 'http://localhost:3000/'
+      },
+      plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+      ],
+      resolveLoader: {
+        modulesDirectories: [
+          path.resolve(__dirname, '../../node_modules')
+        ]
+      },
+      resolve: {
+        extensions: ['', '.js'],
+        root: [
+          path.resolve(__dirname, '../../src/')
+        ]
+      },
+      module: {
+        loaders: [
+        ]
+      }
+    },
+
     configure: function () {
-      var config = sails.config[this.configKey];
+      // Create webpack compiler
+      var compiler = webpack(this.config, function(err, stats) {
+        if (process.env.NODE_ENV === 'development') {
+          // Development: Watch for changes
+          sails.log.info('Watching for changes...');
+          compiler.watch({ aggregateTimeout: 300 }, function (err, stats) {
+            if (err) sails.log.error('Webpack watch failed', err);
+          });
+        }
+        else {
+          // Production: Run production build
+          sails.log.info('Running production build...');
+          compiler.run(function (err, stats) {
+            if (err) sails.log.error('Webpack build failed', err);
+          })
+        }
+      });
+
+      // Start the webpack dev server
+      if (process.env.NODE_ENV === 'development') {
+        this.config.entry.unshift('webpack-dev-server/client?http://localhost:3000/', 'webpack/hot/dev-server');
+        var server = new webpackDevServer(compiler, {
+          contentBase: 'assets',
+          hot: true,
+          quiet: true
+        });
+        server.listen(3000);
+      }
 
       sails.log.verbose("Vue.js hook configured.");
     }
